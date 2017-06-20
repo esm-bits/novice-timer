3575c58d313db0cb4d862646e094d53dd65de22c
shunji-yamaguchi <shunji.yamaguchi@esm.co.jp>
2017-06-20 17:19:02 +0900

    不要な@RestControllerを削除

--- src/main/java/novicetimer/co/jp/novicetimer/NoviceTimerApplication.java
+++ src/main/java/novicetimer/co/jp/novicetimer/NoviceTimerApplication.java
@@ -2,10 +2,8 @@ package novicetimer.co.jp.novicetimer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class NoviceTimerApplication {
     public static void main(String[] args) {
 		SpringApplication.run(NoviceTimerApplication.class, args);
