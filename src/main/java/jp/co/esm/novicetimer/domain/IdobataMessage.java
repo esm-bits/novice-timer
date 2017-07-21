f4e15b05c36ed353f894bd4184fcce0f974b5f9f
shunji-yamaguchi <shunji.yamaguchi@esm.co.jp>
2017-07-21 11:13:21 +0900

    IdobataMessageコンストラクタのインデントを修正

--- src/main/java/jp/co/esm/novicetimer/domain/IdobataMessage.java
+++ src/main/java/jp/co/esm/novicetimer/domain/IdobataMessage.java
@@ -14,8 +14,8 @@ public class IdobataMessage {
     private IdobataMessage(Builder builder) {
         this.source = String.join(
             "",
-            builder.userList.stream().distinct().map(user -> "@" + user + " ").collect(Collectors.toList()))
-            + builder.message;
+            builder.userList.stream().distinct().map(user -> "@" + user + " ").collect(Collectors.toList())
+        ) + builder.message;
     }
 
     public static class Builder {
